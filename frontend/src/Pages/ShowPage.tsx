d6b1df379d5f94ab2a904f705812c92dbd1c5879
Ciaran Grant <ciarangrant2@aol.co.uk>
2023-10-09 02:23:59 +0100

    Fully responsive

--- frontend/src/Pages/ShowPage.tsx
+++ frontend/src/Pages/ShowPage.tsx
@@ -103,10 +103,11 @@ export default function ShowPage(){
             <Typography component="h1" variant='h1' sx={{mb: 4, textAlign: 'center', border: '2px solid white', marginBottom: '30px', padding: '20px', borderRadius: '10px', color: 'white', 
             backgroundImage: "URL('https://images.unsplash.com/photo-1513628253939-010e64ac66cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1500&q=80')",
             backgroundSize: 'cover',
+            fontSize: {xs: '2rem', sm: '3rem', md: '4rem', lg: '5rem', xl: '6rem'}
             }}>{isLoading ? 'Loading...' : item.name}</Typography>
             {
                 isLoading ?
-                <Box sx={{display: 'flex', alignItems: 'center',height: '55vh', mt: '10',}}>
+                <Box sx={{display: 'flex', alignItems: 'center', height: '55vh', mt: '10',}}>
                     <CircularProgress size={'4rem'}/>
                 </Box>:
                 (
@@ -115,92 +116,93 @@ export default function ShowPage(){
                     borderRadius: '10px', color: 'white', 
                     backgroundImage: "URL('https://images.unsplash.com/photo-1513628253939-010e64ac66cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1500&q=80')",
                     backgroundSize: 'cover',
+                    width: {xs: '300px', sm: '500px', md: '800px', lg: '1000px', xl: '1200px'},
                     }}>
                         {item.height && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Height: {item.height}cm</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Height: {item.height}cm</Typography>
                         }
                         {item.mass && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Weight: {item.mass}kg</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Weight: {item.mass}kg</Typography>
                         }
                         {item.hair_color && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Hair Color: {item.hair_color}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Hair Color: {item.hair_color}</Typography>
                         }
                         {item.skin_color && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Skin Color: {item.skin_color}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Skin Color: {item.skin_color}</Typography>
                         }
                         {item.eye_color && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Eye Color: {item.eye_color}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Eye Color: {item.eye_color}</Typography>
                         }
                         {item.birth_year && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Birth Year: {item.birth_year}</Typography>   
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Birth Year: {item.birth_year}</Typography>   
                         }
                         {item.gender && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize' }}>Gender: {item.gender}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Gender: {item.gender}</Typography>
                         }
                         {item.model && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Model: {item.model}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Model: {item.model}</Typography>
                         }
                         {item.manufacturer && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Manufacturer: {item.manufacturer}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Manufacturer: {item.manufacturer}</Typography>
                         }
                         {item.cost_in_credits && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Cost In Credits: {item.cost_in_credits}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Cost In Credits: {item.cost_in_credits}</Typography>
                         }
                         {item.length && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Length: {item.length} meters</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Length: {item.length} meters</Typography>
                         }
                         {item.max_atmosphering_speed && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Max Atmosphering Speed: {item.max_atmosphering_speed}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Max Atmosphering Speed: {item.max_atmosphering_speed}</Typography>
                         }
                         {item.crew && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Crew: {item.crew}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Crew: {item.crew}</Typography>
                         }
                         {item.passengers && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Passengers: {item.passengers}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Passengers: {item.passengers}</Typography>
                         }
                         {item.cargo_capacity && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Cargo Capacity: {item.cargo_capacity}lbs</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Cargo Capacity: {item.cargo_capacity}lbs</Typography>
                         }
                         {item.consumables && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Consumables: {item.consumables}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Consumables: {item.consumables}</Typography>
                         }
                         {item.hyperdrive_rating && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>HyperDrive Rating: {item.hyperdrive_rating}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>HyperDrive Rating: {item.hyperdrive_rating}</Typography>
                         }
                         {item.MGLT && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>MGLT: {item.MGLT}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>MGLT: {item.MGLT}</Typography>
                         }
                         {item.vehicle_class && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Vehicle Class: {item.vehicle_class}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Vehicle Class: {item.vehicle_class}</Typography>
                         }
                         {item.starship_class && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Starship Class: {item.starship_class}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Starship Class: {item.starship_class}</Typography>
                         }
                         {item.rotation_period && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Rotation Period: {item.rotation_period}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Rotation Period: {item.rotation_period}</Typography>
                         }
                         {item.orbital_period && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Orbital Period: {item.orbital_period}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Orbital Period: {item.orbital_period}</Typography>
                         }
                         {item.diameter && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Diameter: {item.diameter}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Diameter: {item.diameter}</Typography>
                         }
                         {item.climate && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Climate: {item.climate}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Climate: {item.climate}</Typography>
                         }
                         {item.gravity && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Gravity: {item.gravity}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Gravity: {item.gravity}</Typography>
                         }
                         {item.terrain && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Terrain: {item.terrain}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Terrain: {item.terrain}</Typography>
                         }
                         {item.surface_water && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Surface Water: {item.surface_water}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Surface Water: {item.surface_water}</Typography>
                         }
                         {item.population && 
-                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize'}}>Population: {item.population}</Typography>
+                            <Typography component="h3" variant="h3" sx={{ mb: 2, color: 'white', textTransform: 'capitalize', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}}}>Population: {item.population}</Typography>
                         }
-                        <Button variant="text" href='/characters' sx={{color: 'white', fontSize: '2rem', fontWeight: '700'}}>Back</Button>
+                        <Button variant="text" href='/characters' sx={{color: 'white', fontSize: {xs: '1rem', sm: '1.5rem', md: '2rem', lg: '2.5rem', xl: '3rem'}, fontWeight: '700'}}>Back</Button>
                     </Box>
                 </Box>
                 )
